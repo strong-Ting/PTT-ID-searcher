33070eb1ff99da13843880f472ce50022f83d01b
strongTing <s8527502987@gmail.com>
2017-11-10 22:25:45 +0800

    -get the href of post

--- js/content.js
+++ js/content.js
@@ -1,16 +1,41 @@
-function get_data()
-{
-    var data = document.querySelector('.gsc-webResult.gsc-result');
-    if(data != null)
-    {
-        clearInterval(num);
-        main(data);
+
+
+const main = (data)=>{
+    for(var i=0;i<data.length;i++){
+        console.log(data[i].querySelector('a.gs-title').href);
     }
 }
-var num = setInterval("var data = get_data();", 100)
 
-function main(data)
-{
-    console.log(data);
+//because not load page in one time,some data can't get.the function to wait the data load
+const query_load = (sel) =>{
+    const query =()=>{
+        let data = document.querySelectorAll(sel);
+        if(data.length!=0){
+            clearInterval(num);
+            main(data);
+        }
+    }
+    query();
 }
+let num = setInterval("query_load('.gsc-webResult.gsc-result')",100);
+
+
+const dom_listenser=(idName,mutationHandler)=>{
+        let target = document.querySelector(idName);
+        let config = { attributes: true, childList: true, characterData: true,subtree:true};
+        let observer = new MutationObserver(mutationHandler);
+        observer.observe(target, config);
+};
+
+/*
+dom_listenser ('head',(mutations)=>{
+    mutations.forEach((mutation)=>{
+        console.log(mutation.target.text);
+        if(mutation.target == 'head'){
+            console.log('have');
+        }
+    });
+
+});
+*/
 
